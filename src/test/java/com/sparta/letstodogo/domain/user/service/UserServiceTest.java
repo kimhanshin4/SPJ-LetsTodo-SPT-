95e9f2b5e44c971101ed9e789d22eb6a4435083e
kimhanshin4 <funold1004@gmail.com>
2023-12-04 11:50:43 +0900

    feat: 사용자 Service Test - 회원가입 미완

---
+++ src/test/java/com/sparta/letstodogo/domain/user/service/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.sparta.letstodogo.domain.user.service;
+
+import static org.mockito.BDDMockito.*;
+
+import com.sparta.letstodogo.user.*;
+import com.sparta.letstodogo.util.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.mockito.*;
+import org.mockito.junit.jupiter.*;
+import org.springframework.security.crypto.password.*;
+
+@DisplayName("사용자 @Service Test")
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+    @Mock
+    JwtUtil jwtUtil;
+
+//    @Test - 미완
+//    @DisplayName("회원가입")
+//    void signup() {
+//        //Given
+//        String username = "SamPorter";
+//        String password = "ReadyToDeliverAPackage";
+//        
+//        UserRequestDto requestDto = new UserRequestDto(username,password);
+//        User user = User.createUser(username,password);
+//        
+//        //When
+//        //Then
+//    }
+}
